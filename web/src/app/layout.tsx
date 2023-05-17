bf7caa7a4099b6acd8cbe5e4d03b6f92e5459b38
charmingruby <mrrubycanal3105@gmail.com>
2023-05-17 12:26:36 -0300

    fonts fixed

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,13 +1,14 @@
 import './globals.css'
 import { ReactNode } from 'react'
-import {
-  Roboto_Flex as Roboto,
-  Bai_Jamjuree as BaiJamjuree,
-} from 'next/font/google'
+import { Roboto, Bai_Jamjuree } from 'next/font/google'
 
-const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+const roboto = Roboto({
+  subsets: ['latin'],
+  variable: '--font-roboto',
+  weight: ['400', '700'],
+})
 
-const baiJamjuree = BaiJamjuree({
+const baiJamjuree = Bai_Jamjuree({
   subsets: ['latin'],
   weight: '700',
   variable: '--font-bai-jamjuree',
