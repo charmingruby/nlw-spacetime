cb677c60622c7c3b7391e94332c23d19ea4ab86f
charmingruby <mrrubycanal3105@gmail.com>
2023-05-18 10:58:50 -0300

    oauth integration and router

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,6 +1,10 @@
 import { StatusBar } from 'expo-status-bar'
 import { ImageBackground, View, Text, TouchableOpacity } from 'react-native'
 import { styled } from 'nativewind'
+import { useEffect } from 'react'
+import { api } from '../src/libs/api'
+import * as SecureStore from 'expo-secure-store'
+import { useRouter } from 'expo-router'
 
 import {
   useFonts,
@@ -9,11 +13,14 @@ import {
 } from '@expo-google-fonts/roboto'
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
-import blurBg from './src/assets/images/bg-blur.png'
-import Stripes from './src/assets/images/stripes.svg'
-import NlwLogo from './src/assets/images/nlw-spacetime-logo.svg'
+import blurBg from '../src/assets/images/bg-blur.png'
+import Stripes from '../src/assets/images/stripes.svg'
+import NlwLogo from '../src/assets/images/nlw-spacetime-logo.svg'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
 
 export default function App() {
+  const router = useRouter()
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
@@ -21,6 +28,44 @@ export default function App() {
   })
 
   const StyledStripes = styled(Stripes)
+
+  const discovery = {
+    authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+    tokenEndpoint: 'https://github.com/login/oauth/access_token',
+    revocationEndpoint:
+      'https://github.com/settings/connections/applications/839c53c87655c9651908',
+  }
+
+  const [, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: '839c53c87655c9651908',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlw-spacetime',
+      }),
+    },
+    discovery,
+  )
+
+  async function handleGithubOAuthCode(code: string) {
+    const response = await api.post('/register', {
+      code,
+    })
+
+    const { token } = response.data
+
+    await SecureStore.setItemAsync('token', token)
+
+    router.push('/memories')
+  }
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params
+
+      handleGithubOAuthCode(code)
+    }
+  }, [response])
 
   if (!hasLoadedFonts) {
     return null
@@ -50,6 +95,7 @@ export default function App() {
         <TouchableOpacity
           activeOpacity={0.7}
           className="rounded-full bg-green-500 px-5 py-2"
+          onPress={() => signInWithGithub()}
         >
           <Text className="font-alt text-sm uppercase text-black">
             CADASTRAR LEMBRANÇA
---
+++ mobile/app/memories.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function Memories() {
+  return (
+    <View className="flex flex-1 items-center justify-center bg-[#121215]">
+      <Text>Memories</Text>
+    </View>
+  )
+}
